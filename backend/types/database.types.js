18ee65b8c34283a9de6c58cdf78d9621d21ca17c
Little :) Biaji <2245076558@qq.com>
2026-02-18 18:59:32 -0500

    udate

--- backend/types/database.types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// ============================================
-// Database Type Definitions
-// Core Tables Only (Simplified Version)
-// ============================================
-Object.defineProperty(exports, "__esModule", { value: true });
